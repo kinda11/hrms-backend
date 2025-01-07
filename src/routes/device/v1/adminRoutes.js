7b998a03db27c0cc79a2b66ca4c58e0af3f0318d
anshuman <anshuman.tiwari@kindasolutions.com>
2025-01-07 10:51:53 +0530

    updated roles

--- src/routes/device/v1/adminRoutes.js
+++ src/routes/device/v1/adminRoutes.js
@@ -4,12 +4,12 @@ const adminController = require("../../../controller/adminController");
 const router = express.Router();
 
 // Admin Routes
-router.get("/admin/employees", authMiddleware, roleMiddleware(["admin"]), adminController.getAllEmployees);
-router.put("/admin/employees/:id", authMiddleware, roleMiddleware(["admin"]), adminController.updateEmployeeRole);
+router.get("/admin/employees", authMiddleware, roleMiddleware(["admin", 'hr', 'manager']), adminController.getAllEmployees);
+router.put("/admin/employees/:id", authMiddleware, roleMiddleware(["admin",'hr', 'manager']), adminController.updateEmployeeRole);
 router.delete("/users/:id", authMiddleware, roleMiddleware(["admin"]), adminController.deleteUser);
 router.get("/admin/employees/details", authMiddleware, roleMiddleware(["admin", "hr"]), adminController.getAllEmployeeDetail);
-router.get("/admin/employees/monthly-details", authMiddleware, roleMiddleware(["admin", 'hr']), adminController.getMonthlyEmployeeDetails);
-router.get('/admin/download-employee-details', authMiddleware, roleMiddleware(["admin", "employee"]), adminController.downloadEmployeeDetails);
-router.get('/admin/dashboard', authMiddleware, roleMiddleware(["admin", "hr"]), adminController.getAdminDashboardData);
+router.get("/admin/employees/monthly-details", authMiddleware, roleMiddleware(["admin", 'hr', 'manager']), adminController.getMonthlyEmployeeDetails);
+router.get('/admin/download-employee-details', authMiddleware, roleMiddleware(["admin", "employee", 'manager', 'hr']), adminController.downloadEmployeeDetails);
+router.get('/admin/dashboard', authMiddleware, roleMiddleware(["admin", "hr", "employee", 'manager',]), adminController.getAdminDashboardData);
 
 module.exports = router;
